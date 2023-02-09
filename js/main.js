5cbac50c0ced2bc5f3e07fdba50f10945f768fc2
Aurelio Oliveira Souza <mulequescbw@gmail.com>
2023-02-09 10:06:57 -0300

    Atualização da Aula03

--- js/main.js
+++ js/main.js
@@ -1,19 +1,32 @@
 const formAdicionaItem = document.getElementById("novoItem")
 const listanoHTML = document.getElementById("lista")
-formAdicionaItem.addEventListener('submit', (evento) => {
-    evento.preventDefault();
-    nomeDoObjeto = evento.target.elements['nome'].value
-    quantidadeDoObjeto = evento.target.elements['quantidade'].value
-    criaElemento(nomeDoObjeto, quantidadeDoObjeto)
+const itens = JSON.parse(localStorage.getItem("itens")) || []
 
+itens.forEach( (elemento) =>{
+    criaElemento(elemento)
 })
 
-function criaElemento(nomeDoElemento, quantidadeDoElemento) {
+formAdicionaItem.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+    nomeDoObjeto = evento.target.elements['nome']
+    quantidadeDoObjeto = evento.target.elements['quantidade']
+    const itemAtual = {
+        "nome": nomeDoObjeto.value,
+        "quantidade": quantidadeDoObjeto.value,
+    }
+    criaElemento(itemAtual)
+    itens.push(itemAtual)
+    localStorage.setItem("itens",JSON.stringify(itens))
+    nomeDoObjeto.value = ""
+    quantidadeDoObjeto.value = "" 
+})
+
+function criaElemento(item) {
     const novoElemento = document.createElement('li')
-    novoElemento.classList.add("item")
     const numeroElemento = document.createElement('strong')
-    numeroElemento.innerHTML = quantidadeDoElemento
+    novoElemento.classList.add("item")
+    numeroElemento.innerHTML = item.quantidade
     novoElemento.appendChild(numeroElemento)
-    novoElemento.innerHTML += nomeDoElemento
+    novoElemento.innerHTML += item.nome
     listanoHTML.appendChild(novoElemento)
 }
